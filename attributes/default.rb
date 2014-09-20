3734e86a873b5964cbbc46956eee2ed515c587a2
Kenton Newby <kentonnewby@gmail.com>
2014-09-20 12:00:19 -0400

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+# Author:: Mix Digital Media
+# Cookbook Name:: basic-security-chef
+# Attributes:: default
+#
+
+# Default to an empty array (e.g. firewall closed).
+# should be popualted in node or role definitions with hashes in
+# the format:
+# {"port": "x", "ip": "xxx.xxx.xxx.xxx"}
+# which means allow traffic on port x from ip xxx.xxx.xxx.xxx
+default[:firewall_allow] = []
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "basic-security-chef"
+maintainer        "Mix Digital Media, LLC"
+maintainer_email  "info@mixdigitalmedia.com"
+description       "Locks down a basic Ubuntu Install. E.g. remove SSH password & root login etc"
+version           "0.0.1"
+
+recipe "basic-security-chef", "Locks down a basic Ubuntu Install"
+
+supports "ubuntu"
